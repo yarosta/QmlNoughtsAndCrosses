8e1f9d6b1b39aa8d28035371558799454c1d9402
Jarosław Starzyński <jaro.starzynski@gmail.com>
2016-05-09 15:11:29 +0200

    Delete noughtsandcrossesview.cpp

--- noughtsandcrossesview.cpp
+++
@@ -1,8 +0,0 @@
-#include "noughtsandcrossesview.h"
-
-NoughtsAndCrossesView::NoughtsAndCrossesView(QObject *parent) : QObject(parent)
-{
-   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
-}
-
